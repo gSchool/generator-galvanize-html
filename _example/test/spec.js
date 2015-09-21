0b9cc6de7384e4f618425fa171d577d9aa2dd3a1
Michael Herman <hermanmu@gmail.com>
2015-09-21 16:48:21 -0600

    updated _example

--- _example/test/spec.js
+++
@@ -1,5 +0,0 @@
-describe('module', function() {
-  it('should do something', function() {
-    expect(true).toEqual(false);
-  });
-});
---
+++ _example/test/test.js
@@ -0,0 +1,9 @@
+var expect = chai.expect;
+// var should = chai.should();
+
+// sample!
+describe('Compare Numbers', function() {
+  it('1 should equal 1', function() {
+    expect(1).to.equal(1);
+  });
+});
