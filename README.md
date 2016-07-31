56d908e823ca606b90edf5e5fcb0f01d4de8fc4f
Michael Herman <hermanmu@gmail.com>
2016-07-31 13:08:32 -0600

    updated to latest version of yeoman

--- README.md
+++ README.md
@@ -8,12 +8,10 @@
 1. Install [Yeoman](http://yeoman.io) (if necessary) - `npm install -g yo`
 1. Install [Gulp](http://gulpjs.com/) (if necessary) - `npm install -g gulp`
 1. Install the generator - `npm install -g generator-galvanize-html`
-1. Run - `yo galvanize-html` and after setting the project up run `npm install`
+1. Run - `yo galvanize-html` and after adding a *.gitignore* run `npm install
+
+> All files must be placed in the "src" directory!
 
 ## Run
 
 1. `gulp`
-
-## Todo
-
-1. Test various iterations of the generator
---
+++ generators/app/index.js
@@ -0,0 +1,85 @@
+(function() {
+
+  'use strict';
+
+  var yeoman = require('yeoman-generator');
+  var chalk = require('chalk');
+  var yosay = require('yosay');
+
+  module.exports = yeoman.Base.extend({
+    prompting: function () {
+      this.log(yosay(
+        'Welcome to the finest ' + chalk.red('generator-galvanize-linter') + ' generator!'
+      ));
+      var prompts = [
+        {
+          type: 'confirm',
+          name: 'angular',
+          message: 'Angular?',
+          default: false
+        }
+      ];
+      this.prompt(prompts, function (props) {
+        this.props = props;
+        done();
+      }.bind(this));
+    },
+    writingHTML: function () {
+      if (this.props.angular) {
+        this.fs.copy(
+          this.templatePath('./src/index2.html'),
+          this.destinationPath('./src/index.html')
+        );
+      } else {
+        this.fs.copy(
+          this.templatePath('./src/index2.html'),
+          this.destinationPath('./src/index.html')
+        );
+      }
+    },
+    writingJavaScript: function () {
+      if (this.props.angular) {
+        this.fs.copy(
+          this.templatePath('./src/js/main-angular.js'),
+          this.destinationPath('./src/js/app.js')
+        );
+      } else {
+        this.fs.copy(
+          this.templatePath('./src/js/main-jquery.js'),
+          this.destinationPath('./src/js/main.js')
+        );
+      }
+    },
+    writingCSS: function () {
+      this.fs.copy(
+        this.templatePath('./src/css/main.css'),
+        this.destinationPath('./src/css/main.css')
+      );
+    },
+    writingMocha: function () {
+      this.fs.copy(
+        this.templatePath('./src/test/index.html'),
+        this.destinationPath('./src/test/index.html')
+      );
+      this.fs.copy(
+        this.templatePath('./src/test/test.js'),
+        this.destinationPath('./src/test/test.js')
+      );
+    },
+    writingFiles: function () {
+      this.fs.copy(
+        this.templatePath('package.json'),
+        this.destinationPath('package.json')
+      );
+      this.fs.copy(
+        this.templatePath('gulpfile.js'),
+        this.destinationPath('gulpfile.js')
+      );
+      this.fs.copy(
+        this.templatePath('.jscsrc'),
+        this.destinationPath('.jscsrc')
+      );
+    }
+  });
+
+}());
