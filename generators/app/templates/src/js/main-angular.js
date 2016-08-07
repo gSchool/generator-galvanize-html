5320203b7b16887cca1177f1790ebfd8fa07b36e
Michael Herman <hermanmu@gmail.com>
2016-08-07 17:10:53 -0600

    updated js files

--- generators/app/templates/src/js/main-angular.js
+++ generators/app/templates/src/js/main-angular.js
@@ -3,5 +3,5 @@
 var app = angular.module('myApp', []);
 
 app.controller('myController', ['$scope', function($scope) {
-  $scope.greeting = "Hello World!";
+  $scope.greeting = 'Hello World!';
 }]);
