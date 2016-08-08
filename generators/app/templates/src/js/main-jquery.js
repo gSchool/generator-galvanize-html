188028f1748f534baaef40d801b20181ab679378
Michael Herman <hermanmu@gmail.com>
2016-08-08 10:34:52 -0600

    es2015 updates

--- generators/app/templates/src/js/main-jquery.js
+++ generators/app/templates/src/js/main-jquery.js
@@ -1,9 +1,12 @@
-// add scripts
-
+// jquery test
 $(document).on('ready', function() {
   console.log('sanity check!');
 });
 
+// es6 test
+const PI = 3.14;
+
+// mocha test
 function sum(num1, num2) {
   return parseInt(num1) + parseInt(num2);
 }
